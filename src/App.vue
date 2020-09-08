7d538421ee9440bc05d682b6fea70ccbf170e38a
ycl <3020651945@qq.com>
2020-09-08 14:51:15 +0800

    完善产品列表的展示效果

--- src/App.vue
+++ src/App.vue
@@ -1,72 +1,153 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-<button id=btn1 @click="testAxios()"> 点击查询</button>
+    <el-card>
+      <el-form :inline="true" :model="dataForm" @keyup.enter.native="testAxios()"  >
+        <el-form-item>
+          <el-col :span="12">
+          <el-input
+            clearable
+            size="small"
+            placeholder="商品名称"
+            v-model="dataForm.name"
+            suffix-icon="el-icon-edit"
+            @click="testAxios()"
+          ></el-input>
+          </el-col>
+        
+          <el-button type="primary" size="small" icon="el-icon-search" @click="testAxios()">查询</el-button>
+        </el-form-item>
+      </el-form>
+      
+    <!-- </el-card> -->
+    <!-- 商品名称：<input type="text" name="selectProduct"><button id=btn1 > 点击查询</button> -->
 
-<el-table :data="goodsList" border>
-      <el-table-column
-        prop="name"
-        label="名称"
-        width="180">
-      </el-table-column>
+    <p></p>
+    <!-- <el-card> -->
+      <el-table :data="goodsList" stripe style="width: 80%">
 
-      <el-table-column
-        prop="price"
-        label="价格"
-        width="180">
-      </el-table-column>
+        <el-table-column prop="name" label="商品名称" align="center"></el-table-column> 
 
-      <el-table-column
-        prop="salePoint"
-        label="卖点"
-        width="180">
-      </el-table-column>
+        <el-table-column prop="price" label="价格" align="center"></el-table-column>
 
-      <el-table-column
-        prop="updateBy"
-        label="修改人"
-        width="180">
-      </el-table-column>
-</el-table>
+        <el-table-column prop="salePoint" label="卖点" align="center"></el-table-column>
+
+        <el-table-column prop="updateBy" label="修改人" align="center"></el-table-column>
+
+        <el-table-column type="expand" label="详情" >
+          
+          <template slot-scope="props">
+            <el-card>
+              <el-form label-position="right" inline class="demo-table-expand">
+                <el-form-item label="主键编号:">
+                  <el-input  size="small" type="text" v-model="props.row.id" ></el-input>
+                </el-form-item>
+                <el-form-item label="商品名称:">
+                  <el-input  size="small" type="text" v-model="props.row.name" ></el-input>
+                </el-form-item>
+                <el-form-item label="商品价格:">
+                  <el-input  size="small" type="text" v-model="props.row.price" ></el-input>
+                </el-form-item>
+                <el-form-item label="卖点:">
+                  <el-input  size="small" type="text" v-model="props.row.salePoint" ></el-input>
+                </el-form-item>
+                <el-form-item label="库存:">
+                  <el-input size="small" type="text" v-model="props.row.stock" ></el-input>
+                </el-form-item>
+                <el-form-item label="商品图片:">
+                  <span>{{ props.row.image }}</span>
+                </el-form-item>
+                
+              </el-form>
+            </el-card>
+          </template>
+          <el-button>修改</el-button>
+        </el-table-column>
+      </el-table>
+    </el-card>
+    <div class="block">
+      <el-pagination
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        :current-page="pageNum"
+        :page-sizes="[5, 10, 20, 50]"
+        :page-size="this.pageSize"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="this.total"
+      ></el-pagination>
+    </div>
   </div>
 </template>
 
 <script>
-import Httpservice from '@/services/HttpService'
+import Httpservice from "@/services/HttpService";
 export default {
-  data () {
-      return {
-        http: Httpservice.getAxios,
-        pageNum:1,
-        pageSize:2,
-        goodsList:[]
+  data() {
+    return {
+      http: Httpservice.getAxios,
+      pageNum: 1,
+      pageSize: 5,
+      goodsList: [],
+      total: 0,
+      dataForm: {
+        name:'',
+        id:''
+
       }
+    };
+  },
+
+  created() {
+    this.testAxios();
+  },
+  methods: {
+    // =========================================
+    testAxios() {
+      let param = {
+        pageNum: this.pageNum,
+        pageSize: this.pageSize,
+      };
+      this.http
+        .get("/product/selectAll", { params: param })
+        .then((response) => {
+          this.goodsList = response.data.rows;
+          this.total = response.data.total;
+        });
     },
-    methods: {
-          testAxios(){
-          let param={
-            pageNum:this.pageNum,
-            pageSize:this.pageSize,
 
-          }
-          this.http.get('/product/selectAll',{params:param}).then((response)=>{
-            console.log(response);
+    // =========================================
+    handleSizeChange(val) {
+      this.pageSize = val;
+      this.testAxios();
+    },
 
-            this.goodsList=response.data.rows
-          })
-        }
-    }
-  
-}
+    // =========================================
+    handleCurrentChange(val) {
+      this.pageNum = val;
+      this.testAxios();
+    },
+  },
+};
 </script>
 
 <style>
+.demo-table-expand {
+  font-size: 0;
+}
+.demo-table-expand label {
+  width: 90px;
+  color: #99a9bf;
+}
+.demo-table-expand .el-form-item {
+  margin-right: 0;
+  margin-bottom: 0;
+  width: 100%;
+}
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  /* font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 60px; */
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -4,12 +4,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 import axios from 'axios'
 
 
 
 Vue.component('axios', axios); 
-Vue.use(ElementUI)
+Vue.use(ElementUI);
 
 
 
